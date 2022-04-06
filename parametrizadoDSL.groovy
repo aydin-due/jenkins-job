549f5adca29ec7bbed186bacc11c5db8977d3e0c
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2022-04-05 23:30:34 -0700

    Create parametrizadoDSL.groovy

---
+++ parametrizadoDSL.groovy
@@ -0,0 +1,41 @@
+job('job-DSL-1') {
+	description('Job DSL de ejemplo para el curso de Jenkins')
+  	scm {
+      git('https://github.com/aydin-due/jenkins-job.git', 'main') { node ->
+        node / gitConfigName('aydin-due')
+        node / gitConfigEmail('a219201085@unison.mx')
+      }
+    }
+  	parameters {
+      stringParam('nombre', defaultValue = 'Julian', description = 'Parametro de cadena para el job booleano')
+      choiceParam('planeta', ['Mercurio', 'Venus', 'Tierra', 'Marte', 'Jupiter', 'Saturno', 'Urano', 'Neptuno'])
+      booleanParam('agente', false)
+    }
+    triggers {
+      cron('H/7 * * * *')
+      githubPush()
+    }
+  	steps {
+      shell("bash jobscript.sh")
+    }
+    publishers {
+      mailer('aydinbdsalman@gmail.com', true, true)
+      slackNotifier {
+        notifyAborted(true)
+        notifyEveryFailure(true)
+        notifyNotBuilt(false)
+        notifyUnstable(false)
+        notifyBackToNormal(true)
+        notifySuccess(false)
+        notifyRepeatedFailure(false)
+        startNotification(false)
+        includeTestSummary(false)
+        includeCustomMessage(false)
+        customMessage(null)
+        sendAs(null)
+        commitInfoChoice('NONE')
+        teamDomain(null)
+        authToken(null)
+      }
+    }
+}
